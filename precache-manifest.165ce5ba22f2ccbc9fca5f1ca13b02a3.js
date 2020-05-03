727d825cba386331c94b66b171286141b789a351
firdauscomel <bilbofirdaus@gmail.com>
2020-05-04 03:21:55 +0800

    Updates

---
+++ precache-manifest.165ce5ba22f2ccbc9fca5f1ca13b02a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6644f39e3b536d0950c981c8a890439c",
+    "url": "/flexbox/index.html"
+  },
+  {
+    "revision": "8d95ccd05ce357742acc",
+    "url": "/flexbox/static/css/main.98fd3374.chunk.css"
+  },
+  {
+    "revision": "59532c963700d50fe102",
+    "url": "/flexbox/static/js/2.7fe8c080.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/flexbox/static/js/2.7fe8c080.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d95ccd05ce357742acc",
+    "url": "/flexbox/static/js/main.6768e60c.chunk.js"
+  },
+  {
+    "revision": "9d04457c755a74dc948b",
+    "url": "/flexbox/static/js/runtime-main.1e484d7d.js"
+  }
+]);
